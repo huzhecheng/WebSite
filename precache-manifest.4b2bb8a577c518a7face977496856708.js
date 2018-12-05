9dda8d7640330fe6ab7282b00fbf322b957f874c
Mrs Fred <fred@Mrsde-MacBook-Pro.local>
2018-12-05 11:58:12 +0800

    Updates

---
+++ precache-manifest.4b2bb8a577c518a7face977496856708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9392e5f0526a2027202c6a9a2508852",
+    "url": "/WebSite/static/media/fred.d9392e5f.png"
+  },
+  {
+    "revision": "605644c53e0d9d41d5fc",
+    "url": "/WebSite/static/js/runtime~main.605644c5.js"
+  },
+  {
+    "revision": "549485e48f321cbca5a7",
+    "url": "/WebSite/static/js/main.549485e4.chunk.js"
+  },
+  {
+    "revision": "12c2c600e0f66bb7007b",
+    "url": "/WebSite/static/js/1.12c2c600.chunk.js"
+  },
+  {
+    "revision": "549485e48f321cbca5a7",
+    "url": "/WebSite/static/css/main.79b1f6de.chunk.css"
+  },
+  {
+    "revision": "ec590825d5c02ae2fd5d8162c6a64ab2",
+    "url": "/WebSite/index.html"
+  }
+];
